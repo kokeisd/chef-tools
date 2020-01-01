a18ab49139737888b8807aeaab32ca34076ba766
Kelvin MK Ko <Kelvin MK Ko@fp.manulife.hk>
2020-01-01 14:19:51 +0800

    updated knife command parameter

--- addchefrecipe.sh
+++ addchefrecipe.sh
@@ -32,14 +32,18 @@ then
 fi
 
 if [ -z "${RECIPE}" ]; then
-  err "No recipe name specifid."
+  error "No recipe name specifid."
+  exit 1
+fi
+
+if [[ ! -f "/etc/chef/client.pem" ]]; then 
+  error "This machine has not been bootstrapped to Chef."
   exit 1
 fi
 
 # Generate the knife config file on-the-fly
 echo "Generating knife.rb"
 [[ -d /etc/chef ]] &&  mkdir /etc/chef/.chef || (error "Error in creating .chef"; exit 1;)
-cd /etc/chef
 readonly CHEF_SERVER_URL=$(grep chef_server_url /etc/chef/client.rb)
 echo "
 current_dir = File.dirname(__FILE__)
@@ -49,15 +53,15 @@ node_name                \"${NODENAME}\"
 client_key               \"/etc/chef/client.pem\"
 ${CHEF_SERVER_URL}
 cookbook_path            [\"#{current_dir}/../cookbooks\"]
-" > ./.chef/knife.rb
+" > /etc/chef/.chef/knife.rb
 
 
-if ! knife cookbook show ${RECIPE}  >/dev/null 2>&1 ; then
+if ! knife cookbook show ${RECIPE} -c /etc/chef/.chef/knife.rb >/dev/null 2>&1 ; then
   error "Recipe ${RECIPE} not found. Exiting."
   exit 1
 fi
 
-if knife node run_list add $NODENAME recipe[$RECIPE] ; then
+if knife node run_list add $NODENAME recipe[$RECIPE] -c /etc/chef/.chef/knife.rb ; then
   echo "${RECIPE} has been added to the runlist"
 else
   error "Failed to add ${RECIPE} to the runlist"
